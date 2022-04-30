67f1832c59402aaa6e833afbd4e653267074506b
Michael Weimann <mail@michael-weimann.eu>
2022-04-30 09:39:41 +0200

    Tweak aliases

--- roles/bash/files/.bashrc.d/aliases.sh
+++ roles/bash/files/.bashrc.d/aliases.sh
@@ -2,13 +2,14 @@ alias ll='ls -alh'
 alias llt='ls -alht'
 
 alias gg='cd ~/git/$(fd --base-directory ~/git --type d --max-depth 2 | fzf)'
-alias ggv='gg && vi'
+alias vg='gg && vi'
 
 alias gid='git diff'
 alias gip='git pull'
 alias gis='git status'
 
-alias vidot='cd ~/git/dotfiles/ && vi'
+alias gdot='cd ~/git/ansible-workstation'
+alias vidot='cd ~/git/ansible-workstation && vi'
 
 # npm
 alias npr='test -f package.json && cat package.json | jq .scripts | jq -r "keys[]" | fzf | xargs -t -r -n1 npm run'
