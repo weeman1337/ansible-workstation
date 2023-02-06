bebfa98ee6f36bc624328413db70fa688fb52815
Michael Weimann <mail@michael-weimann.eu>
2023-02-06 18:33:32 +0100

    Enable tree-sitter

--- roles/neovim/files/config/lua/weeman/plugins.lua
+++ roles/neovim/files/config/lua/weeman/plugins.lua
@@ -147,49 +147,36 @@ return require('packer').startup(function (use)
         end
     }
 
-    --use {
-      --"nvim-treesitter/nvim-treesitter",
-      --requires = {
-        --"nvim-treesitter/playground",
-        --"nvim-treesitter/nvim-treesitter-textobjects",
-      --},
-      --run = ':TSUpdate',
-      --config = function()
-        --require'nvim-treesitter.configs'.setup {
-          --ensure_installed = "all",
-          --highlight = {
-            --enable = false,
-          --},
-          --textobjects = {
-            --select = {
-              --enable = true,
-              --keymaps = {
-                --["af"] = "@function.outer",
-                --["if"] = "@function.inner",
-              --},
-            --},
-          --},
-          --playground = {
-            --enable = true,
-            --disable = {},
-            --updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-            --persist_queries = false, -- Whether the query persists across vim sessions
-            --keybindings = {
-              --toggle_query_editor = 'o',
-              --toggle_hl_groups = 'i',
-              --toggle_injected_languages = 't',
-              --toggle_anonymous_nodes = 'a',
-              --toggle_language_display = 'I',
-              --focus_language = 'f',
-              --unfocus_language = 'F',
-              --update = 'R',
-              --goto_node = '<cr>',
-              --show_help = '?',
-            --},
-          --}
-        --}
-      --end
-    --}
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      requires = {
+        "nvim-treesitter/playground",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+      },
+      run = ':TSUpdate',
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          ensure_installed = {
+            "javascript",
+            "lua",
+            "markdown",
+            "typescript",
+          },
+          highlight = {
+            enable = false,
+          },
+          textobjects = {
+            select = {
+              enable = true,
+              keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+              },
+            },
+          },
+        }
+      end
+    }
 
     use {
         "hrsh7th/nvim-cmp",
@@ -597,4 +584,4 @@ return require('packer').startup(function (use)
     if PACKER_BOOTSTRAP then
       require('packer').sync()
     end
-end)
+  end)
