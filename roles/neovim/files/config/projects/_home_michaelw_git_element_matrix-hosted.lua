309877af9189d80884aadca4a3f48a70c6fc1516
Michael Weimann <michaelw@element.io>
2023-06-16 08:37:59 +0200

    Add hosted project config

---
+++ roles/neovim/files/config/projects/_home_michaelw_git_element_matrix-hosted.lua
@@ -0,0 +1,12 @@
+local _M = {}
+
+_M.format_on_save = {
+  javascript = true,
+  javascriptreact = true,
+  markdown = true,
+  scss = true,
+  typescript = true,
+  typescriptreact = true,
+}
+
+return _M
